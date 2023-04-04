4315b29f609b2c03e6365a7895b5c2f5f41842c4
Witcher-MTM <phantomit228@gmail.com>
2023-04-04 18:58:07 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-<<<<<<< Updated upstream
-=======
 const OrderRouter = require("./Routes/OrderRouter")
 const app = express();
 const cors = require('cors');
@@ -9,14 +7,10 @@ const corsOptions = {
   origin: 'http://localhost:3001',
   optionsSuccessStatus: 200
 }
-
 app.use(express.json());
-
 app.use(cors(corsOptions));
->>>>>>> Stashed changes
 
-const app = express();
-
+app.use('/order',OrderRouter);
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
