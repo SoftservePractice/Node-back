436f26bb2c5b480f753c865dce3f63c42218c291
Witcher-MTM <phantomit228@gmail.com>
2023-04-12 11:45:14 +0300

    Update OrderRouter.js

--- server/Routes/OrderRouter.js
+++ server/Routes/OrderRouter.js
@@ -9,7 +9,6 @@ app.post('/',async (req,res)=>{
 })
 app.get('/activate/:token/:id',async (req,res)=>{
    await ClientController.ConfirmEmail(req,res);
-   res.send('Confirmed');
 })
 
 module.exports = app;
