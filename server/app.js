173f18a398347756eb7d2d5d6e8e1cce5a115827
vovkasWeb <vova77557@gmail.com>
2023-04-13 17:54:56 +0300

    add: email push record, remiders, report

--- server/app.js
+++ server/app.js
@@ -8,11 +8,19 @@ app.use(express.json());
 const server = async () => {
  const linkTo= "orlovsky.top@gmail.com";
   const activationLink = await bcrypt.hash(linkTo, 8);
-  console.log(activationLink);
-  await mailService.sendActivationMail(
-    linkTo,
-    `http://localhost:5001/order/activate/${activationLink}`
-  );
+  // console.log(activationLink);
+  // await mailService.sendActivationMail(
+  //   linkTo,
+  //   `http://localhost:5001/order/activate/${activationLink}`
+  // );
+
+//  await mailService.sendRecordMail(linkTo, "18:30 12.04.2023",'Днепр кукурузина 1',"+38000000000000","carservice22945@gmail.com")
+//  await mailService.sendRemindersMail(linkTo, "18:30 12.04.2023",'Днепр кукурузина 1',"+38000000000000","carservice22945@gmail.com");
+ await mailService.sendReportMail(linkTo, [
+  {name: "мотор жыгулей", priceDetail: 100, priceRepair:200},
+  {name: "коробка жыгулей", priceDetail: 201, priceRepair:99}
+]);
+
 };
 
 
--- server/mail-sevise.js
+++ server/mail-sevise.js
@@ -27,5 +27,100 @@ class MailService {
       `
     });
   }
+  async sendRecordMail(to,time, adress,phone,email ) {
+    await this.transporter.sendMail({
+      from: "avtoservis2234@gmail.com",
+      to,
+      subject: "Заявка на автосервіс",
+      text:"",
+      html:
+      `<div style="font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: black;">
+      <h1 style="padding-top: 30px; text-align: center; font-size: 32px; font-weight: 900;">Сar-service</h1>
+      <p style="text-align: center; font-weight: 900; font-size: 38px; padding:40px; background-color: rgb(53, 121, 174);">Вашу заявку успішно створено</p>
+      <div style="padding-left: 20px;">
+      <span style="font-size: 25px; font-weight: 900;">Дані щодо запису:</span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Дата: <span style="font-weight: 900;">${time}</span></span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Адреса: <span style="font-weight: 900;">${adress}</span></span>
+      <span style="display: block; font-weight: 900; font-size: 32px;padding-bottom: 10px;">Залишилися питання щодо запису?</span>
+      <span style="font-size: 25px; font-weight: 900;">Контактні дані:</span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Телефон: <span style="font-weight: 900;">${phone}<span></span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Email: <span style="font-weight: 900;">${email}<span></span>
+       </div>
+      <h3 style="font-size: 32px; font-weight: 900; text-align: center;">Дякуемо за вашу заявку!</h3>
+      </div>
+      `
+    });
+  }
+  
+  async sendRemindersMail(to,time, adress,phone,email ){
+    await this.transporter.sendMail({
+      from: "avtoservis2234@gmail.com",
+      to,
+      subject: "Нагадування про заявку на автосервіс",
+      text:"",
+      html:
+      `<div style="font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: black;">
+      <h1 style="padding-top: 30px; text-align: center; font-size: 32px; font-weight: 900;">Сar-service</h1>
+      <p style="text-align: center; font-weight: 900; font-size: 38px; padding:40px; background-color: rgb(53, 121, 174);">Нагадування про запис в автосервіс</p>
+      <div style="padding-left: 20px;">
+      <span style="font-size: 25px; font-weight: 900;">Дані щодо запису:</span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Дата: <span style="font-weight: 900;">${time}</span></span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Адреса: <span style="font-weight: 900;">${adress}</span></span>
+      <span style="display: block; font-weight: 900; font-size: 32px;padding-bottom: 10px;">Залишилися питання щодо запису?</span>
+      <span style="font-size: 25px; font-weight: 900;">Контактні дані:</span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Телефон: <span style="font-weight: 900;">${phone}<span></span>
+      <span style="display: block; font-size: 18px; margin: 5px 0;">Email: <span style="font-weight: 900;">${email}<span></span>
+       </div>
+      <h3 style="font-size: 32px; font-weight: 900; text-align: center;">Дякуемо що ви з нами!</h3>
+      </div>
+      `
+    });
+  }
+  async sendReportMail(to,arr ){
+    let totalPrice =0;
+    await this.transporter.sendMail({
+      from: "avtoservis2234@gmail.com",
+      to,
+      subject: "Нагадування про заявку на автосервіс",
+      text:"",
+      html:
+      ` 
+      <div style="font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: black;">
+    <h1 style="padding-top: 30px; text-align: center; font-size: 32px; font-weight: 900;">Сar-service</h1>
+    <p style="text-align: center; font-weight: 900; font-size: 38px; padding:40px; background-color: rgb(53, 121, 174);">Звіт про виконані роботи</p>
+    <div style="padding-left: 20px;">
+        <table style="border: 1px solid black;border-collapse: collapse; width: 75%; margin: 0 auto;">
+            <tr>
+              <th style="border: 1px solid black;">Назва</th>
+              <th style="border: 1px solid black;">Ціна деталей</th>
+              <th style="border: 1px solid black;">ціна роботи</th>
+            </tr>
+            ${ arr.map( item => {
+               totalPrice += item.priceDetail + item.priceRepair;
+              return`
+                <tr>
+                <td style="border: 1px solid black; padding-left: 10px; font-weight: 900; font-size: 18px;">${item.name}</td>
+                <td style="border: 1px solid black; padding-left: 10px; font-weight: 900; font-size: 18px;">uan:${item.priceDetail}</td>
+                <td style="border: 1px solid black; padding-left: 10px; font-weight: 900; font-size: 18px;">uan:${item.priceRepair}</td>
+            </tr>`
+            }
+            ).join('')
+            }
+            <tr>
+                <td style="border: 1px solid black;font-weight:700;padding: 5px 0 5px 10px; font-size: 19px;" >Oканчательная цена: <span  style=" font-size: 20px;font-weight: 900; padding-left: 5px;">${totalPrice}</span> </td>
+            </tr>
+        </table>
+        <div style="padding-top: 10px;">
+               <span style="font-size: 25px; font-weight: 900; margin-top: 10px;">Контактні дані:</span>
+               <span style="display: block; font-size: 18px; margin: 5px 0;">Телефон: <span style="font-weight: 900;">+38000000000<span></span>
+               <span style="display: block; font-size: 18px; margin: 5px 0;">Email: <span style="font-weight: 900;">urkdeee@gmail.com<span></span>
+        </div>
+    </div>
+
+    <h3 style="font-size: 32px; font-weight: 900; text-align: center;">Дякуемо що ви з нами!</h3>
+</div>
+      `
+    });
+  }
 }
 module.exports = new MailService();
